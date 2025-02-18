e07598de6cdbac374d2bdd81f644645385f59a1d
Titiplex <107863779+Titiplex@users.noreply.github.com>
2025-02-18 20:31:30 +0100

    Config classes

--- src/main/java/com/aixuniversity/maasaidictionary/config/DaoConfig.java
+++ src/main/java/com/aixuniversity/maasaidictionary/config/DaoConfig.java
@@ -2,6 +2,9 @@ package main.java.com.aixuniversity.maasaidictionary.config;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Properties;
 
 /**
@@ -22,7 +25,6 @@ public abstract class DaoConfig {
         }
     }
 
-    // Méthode d'accès
     public static String get(String key) {
         return PROPERTIES.getProperty(key);
     }
@@ -30,5 +32,32 @@ public abstract class DaoConfig {
     // Surcharge si vous voulez des valeurs par défaut
     public static String get(String key, String defaultValue) {
         return PROPERTIES.getProperty(key, defaultValue);
+    }
+
+    /**
+     * Retourne le nom de la table associée à une entité (ex: "users" -> "USERS").
+     */
+    public static String getTableName(String key) {
+        return PROPERTIES.getProperty(key + ".tableName");
+    }
+
+    /**
+     * Retourne la liste des colonnes associées à une entité (ex: "users").
+     */
+    public static List<String> getColumns(String key) {
+        String columns = PROPERTIES.getProperty(key + ".columns");
+        if (columns == null) return Collections.emptyList();
+        return Arrays.asList(columns.split(","));
+    }
+
+    /**
+     * Retourne le nom réel d'une colonne en base de données.
+     */
+    public static String getColumnName(String key, String columnKey) {
+        return PROPERTIES.getProperty(key + ".column." + columnKey);
+    }
+
+    public static String getColumnType(String key, String columnKey) {
+        return PROPERTIES.getProperty(key + ".type." + columnKey);
     }
 }
--- src/main/java/com/aixuniversity/maasaidictionary/config/SqlStringConfig.java
+++ src/main/java/com/aixuniversity/maasaidictionary/config/SqlStringConfig.java
@@ -1,4 +1,52 @@
 package main.java.com.aixuniversity.maasaidictionary.config;
 
-public class SqlStringConfig {
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class SqlStringConfig {
+    private static final String INSERTION_STRING = "insert into ";
+    private static final String SELECTION_STRING = "select * from ";
+    private static final String DELETION_STRING = "delete from ";
+    private static final String UPDATE_STRING = "update ";
+
+    public static String getInsertionString(String key) {
+        String insertionString = INSERTION_STRING;
+        List<String> columnsNames = new ArrayList<>();
+        List<String> placeholders = new ArrayList<>();
+        for (String column : DaoConfig.getColumns(key)) {
+            columnsNames.add(DaoConfig.getColumnName(key, column));
+        }
+        for (int i = 0; i < columnsNames.size(); i++) {
+            placeholders.add("?");
+        }
+        insertionString += DaoConfig.getTableName(key) + " (" + String.join(", ", columnsNames) + ")"
+        + " values (" + String.join(", ", placeholders) + ")" ;
+
+        return insertionString;
+    }
+
+    public static String getSelectionAllString(String key) {
+        return SELECTION_STRING + DaoConfig.getTableName(key);
+    }
+
+    public static String getSelectionStringById(String key, int id) {
+        return SELECTION_STRING + DaoConfig.getTableName(key) + " where id=" + id;
+    }
+
+    public static String getDeletionString(String key, int id) {
+        return DELETION_STRING + DaoConfig.getTableName(key) + " where id=" + id;
+    }
+
+    public static String getUpdateString(String key, int id) {
+        StringBuilder updateString = new StringBuilder(UPDATE_STRING + DaoConfig.getTableName(key) + " set ");
+
+        List<String> columns = DaoConfig.getColumns(key);
+        for (String col : columns) {
+            String column = DaoConfig.getColumnName(key, col);
+            updateString.append(column).append(" =? ");
+        }
+        updateString.append(" where id=").append(id);
+
+        return updateString.toString();
+    }
 }
